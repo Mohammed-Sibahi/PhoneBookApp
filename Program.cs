1f1645927d2365da90be35bc49b1144e7a710105
Sibahi <sibahi@live.com>
2022-03-20 01:14:49 +0400

    create the userInput variable

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace PhoneBookApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the PhoneBookApp!");
+            Console.WriteLine("Select operation");
+            Console.WriteLine("1 Add contact");
+            Console.WriteLine("2 Display contact by number");
+            Console.WriteLine("3 View all contacts");
+            Console.WriteLine("4 serach for contacts for a given name.")
+
+            var userInput = Console.ReadLine();        
         }
     }
 }
